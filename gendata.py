b81a63ee12b95d64fa66404feb4ef3e924fbb8c7
Sarthak <successar@gmail.com>
2017-05-04 16:41:10 +0530

    Good commit

---
+++ gendata.py
@@ -0,0 +1,29 @@
+
+def gendata() :
+    data_dir = 'input/ATIS_samples/'
+    f = ['train', 'test', 'valid']
+    ext = ['.ids10000.seq.in', '.ids123.seq.out', '.ids.labels']
+    output = {k:[] for k in f}
+    for n in f :
+        for e in ext :
+            fp = open(data_dir + n + '/' + n + e).readlines()
+            fp = [x.strip() for x in fp]
+            if 'labels' not in e :
+                fp = [x.split(' ') for x in fp]
+                fp = [[int(a) for a in x] for x in fp]
+            else :
+                fp = [int(x) for x in fp]
+            output[n].append(fp)
+
+        output[n] = tuple(output[n])
+
+    f = open(data_dir + 'in_vocab_10000.txt').readlines()
+    output['idx2words'] = {i:f[i].strip() for i in range(len(f))}
+
+    f = open(data_dir + 'out_vocab_123.txt').readlines()
+    output['idx2labels'] = {i:f[i].strip() for i in range(len(f))}
+
+    f = open(data_dir + 'labels.txt').readlines()
+    output['idx2intents'] = {i:f[i].strip() for i in range(len(f))}
+    return output
+
---
+++ jointslu.py
@@ -0,0 +1,93 @@
+import numpy as np
+from keras.models import Model
+from keras.layers.recurrent import LSTM
+from keras.layers.core import Lambda, Flatten
+from keras.layers import Activation, Dense, Input
+from keras.layers.wrappers import TimeDistributed, Bidirectional
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers.embeddings import Embedding
+from gendata import gendata
+
+from RecurrentNeuralNetwork import conlleval, get_perf
+
+dataset = gendata()
+trainSentences, trainY, trainL = dataset['train']
+validSentences, validY, validL = dataset['valid']
+testSentences,  testY, testL  = dataset['test']
+idx2labels = dataset['idx2labels']
+idx2words = dataset['idx2words']
+idx2intents = dataset['idx2intents']
+
+lengths = [len(x) for x in trainSentences]
+print 'Input sequence length range: ', max(lengths), min(lengths)
+
+maxlen = max(lengths)
+print 'Maximum sequence length:', maxlen
+
+from keras.utils.np_utils import to_categorical
+
+X_train = pad_sequences(trainSentences, maxlen=maxlen)
+y_train = pad_sequences(trainY, maxlen=maxlen)
+y_train = np.array([to_categorical(x, num_classes=len(idx2labels)) for x in y_train])
+l_train = np.array(to_categorical(trainL, num_classes=len(idx2intents)))
+
+X_valid = pad_sequences(validSentences, maxlen=maxlen)
+y_valid = pad_sequences(validY, maxlen=maxlen)
+y_valid = np.array([to_categorical(x, num_classes=len(idx2labels)) for x in y_valid])
+l_valid = np.array(to_categorical(validL, num_classes=len(idx2intents)))
+
+X_test = pad_sequences(testSentences, maxlen=maxlen)
+y_test = pad_sequences(testY, maxlen=maxlen)
+y_test = np.array([to_categorical(x, num_classes=len(idx2labels)) for x in y_test])
+
+
+from keras.constraints import maxnorm
+from keras import regularizers
+vocab_size = len(idx2words)
+embedding_size = 300
+hidden_size = 128
+slot_size = len(idx2labels)
+intent_size = len(idx2intents)
+
+main_input = Input(shape=(maxlen,), dtype='int32', name='main_input')
+embed = Embedding(vocab_size, embedding_size, input_length=maxlen, mask_zero=True)(main_input)
+lstm = Bidirectional(LSTM(hidden_size, return_sequences=True, kernel_constraint=maxnorm(3.)), merge_mode='concat')(embed)
+slots = TimeDistributed(Dense(slot_size, kernel_constraint=maxnorm(3.), kernel_regularizer=regularizers.l2(0.0001)))(lstm)
+sliced = Lambda(lambda x : x[:, -1, :])(lstm)
+intents = Dense(intent_size, kernel_constraint=maxnorm(3.), kernel_regularizer=regularizers.l2(0.0001))(sliced)
+#intents = TimeDistributed(Dense(intent_size))(lstm)
+outputslots = Activation('softmax', name='so')(slots)
+outputintents = Activation('softmax', name='io')(intents)
+
+model = Model(inputs=[main_input], outputs = [outputslots, outputintents])
+
+print "compiling"
+model.compile(loss='categorical_crossentropy', optimizer='adam', loss_weights=[0.6, 0.4], metrics={'io' : 'accuracy'})
+
+batch_size = 32
+
+print "fitting"
+
+def remove_padding(pred, actu) :
+    output = []
+    for a,b in zip(pred, actu) :
+        if b != 0 :
+            output.append(a)
+
+    return output
+
+wordsValid = [map(lambda x: idx2words[x], w) for w in validSentences]
+validAct = [map(lambda x: idx2labels[x], y) for y in validY]
+wordsTest = [map(lambda x: idx2words[x], w) for w in testSentences]
+testAct = [map(lambda x: idx2labels[x], y) for y in testY]
+
+def predict_classes(x) :
+    pred = model.predict(x)
+    return pred[0].argmax(axis=-1)
+
+while(True) :
+    model.fit([X_train], [y_train, l_train], batch_size=batch_size, nb_epoch=1, validation_data=([X_valid], [y_valid, l_valid]))
+    validPred = [map(lambda x: idx2labels[x], remove_padding(c, d)) for c, d in zip(predict_classes([X_valid]), X_valid)]
+    print conlleval(validPred, validAct, wordsValid, 'output/current.valid.txt')
+    testPred = [map(lambda x: idx2labels[x], remove_padding(c, d)) for c, d in zip(predict_classes([X_test]), X_test)]
+    print conlleval(testPred, testAct, wordsTest, 'output/current.test.txt')
